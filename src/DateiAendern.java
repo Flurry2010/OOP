b3f318694a5220dc8cf1d9b986dcb3b8e773dc71
Flurry2010 <flurry2010@gmail.com>
2015-09-28 07:21:08 +0200

    HA02

--- src/DateiAendern.java
+++ src/DateiAendern.java
@@ -1,7 +1,4 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by dfleuren on 25.09.2015.
@@ -10,50 +7,61 @@ public class DateiAendern {
 
     public void hexEinlesen(String bild) {
 
-        //long start = System.currentTimeMillis();
-        try (BufferedInputStream input = new BufferedInputStream(
-                new FileInputStream(bild))) {
+        File fileOut = new File("neu.txt");
 
-            File file = new File(bild);
+        try (BufferedInputStream input = new BufferedInputStream(
+                new FileInputStream(bild)); BufferedWriter bw = new BufferedWriter(new FileWriter(fileOut))) {
+
+            File fileIn = new File(bild);
             byte[] b = new byte[16];
             int a;
             int add = 0;
-            int maxAdd = Long.toHexString(file.length()).length();
+            int maxAdd = Long.toHexString(fileIn.length()).length();
 
             while ((a = input.read(b)) != -1) {
                 StringBuilder sb = new StringBuilder(Integer.toHexString(add).toUpperCase());
 
                 while (sb.length() < maxAdd)
-                    sb.insert(0,"0");
+                    sb.insert(0, "0");
 
                 sb.append(":  ");
 
-                int count = 0;
                 for (int i = 0; i < a; i++) {
                     int hex = b[i] & 255;
                     String hexS = Integer.toHexString(hex).toUpperCase();
                     if (hexS.length() == 1) hexS = "0" + hexS;
-                    sb.append(hexS + " ");
-                    count++;
+                    sb.append(hexS).append(" ");
+
+                }
+                if (a < 16) {
+                    byte[] temp = new byte[a];
+
+                    for (int i = 0; i < a; i++) {
+                        temp[i] = b[i];
+                    }
+
+                    while (a < 16) {
+                        sb.append("   ");
+                        a++;
+                    }
+
+                    sb.append(" ");
+                    sb.append(new String(b).replaceAll("[^\\p{Print}]", ".")); //regular expression (p ist ein set (print nur druckbare Zeichen)
+                    System.out.println(sb);
+                    add += 16;
                 }
 
-                while (count < 16) {
-                    sb.append("   ");
-                    b[a] = 0;
-                    count++;
-                }
-
-                sb.append(" " + new String(b).replaceAll("[^\\p{Print}]", ".")); //regular expression (p ist ein set (print nur druckbare Zeichen)
-                System.out.println(sb);
-                add += 16;
             }
-
-        } catch
-                (IOException e) {
-            System.out.println("Fehler beim lesen....");
+            }catch
+            (IOException e){
+                System.out.println("Fehler beim lesen....");
+            }
+            //System.out.println(System.currentTimeMillis() - start);
         }
-        //System.out.println(System.currentTimeMillis() - start);
+
+
     }
 
 
-}
+
+
