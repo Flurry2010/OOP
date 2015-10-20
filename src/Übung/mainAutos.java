f3705568a0ecdefe46bf6bbc64348fb4fc4bac2d
Flurry2010 <flurry2010@gmail.com>
2015-10-20 15:38:39 +0200

    Übung

---
+++ src/Übung/mainAutos.java
@@ -0,0 +1,33 @@
+package Übung;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dfleuren on 20.10.2015.
+ */
+public class mainAutos {
+    public static void main(String[] args) {
+
+        Autos a = new Autos();
+        List<File> addr = new ArrayList<>();
+        int count = 0;
+
+        System.out.println("Dateien werden eingelesen");
+
+        for (int i = 0; i <= 32; i++) {
+            String daten = "Y:\\3_XI\\XI_6\\302_SOP_OOP\\Autos\\Autos_" + i + ".txt";
+            File file = new File(daten);
+            if (file.exists()) {
+                addr.add(file);
+                System.out.print(".");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("Es wurden " + count + " Dateien gefunden" + "\n");
+        System.out.println("Daten werden verarbeitet");
+        a.einlesen(addr);
+    }
+}
