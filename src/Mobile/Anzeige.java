ce6647d6e4a65b643200d48d8fa3d3ae1d42c593
Flurry2010 <Flurry2010@gmail.com>
2015-11-17 19:01:40 +0100

    HA Layout fertig

--- src/Mobile/Anzeige.java
+++ src/Mobile/Anzeige.java
@@ -4,6 +4,7 @@ package Mobile;
 
 import GUI.JavaFrame;
 import GUI.Layout;
+import javafx.scene.layout.Background;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -60,7 +61,7 @@ public class Anzeige{
 
     public void menu(List<Auto> autos){
 
-        JFrame window = new JFrame("Das Menü");
+        JFrame window = new JFrame("Das Menü zur Autosortierung !!!");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Anlegen des Model mit JList
@@ -71,28 +72,36 @@ public class Anzeige{
             listModel.addElement(a.toHTML());
         }
 
+        JTextArea ta = new JTextArea("dick Titten");
+
         // SrcollPanel erstelen
-        JScrollPane listeSrcoller = new JScrollPane(list);
+        JScrollPane listeSrcoller = new JScrollPane(ta);
 
         // Anlegen von JPanel
         JPanel links = new JPanel();
         JPanel rechts = new JPanel();
         JPanel unten = new JPanel();
 
+        //Rahme für die Panels
+        links.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+        rechts.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+        unten.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+
         // Neues Layout erzwingen
-        links.setLayout(new GridLayout(1,3));
+        links.setLayout(new GridLayout(4, 1));
+        rechts.setLayout(new GridLayout(4, 1));
 
         // Anlegen von Buttons
-        JButton nameB = new JButton();
-        JButton preisB = new JButton();
-        JButton ezB = new JButton();
-        JButton kmB = new JButton();
+        JButton nameB = new JButton("NAME");
+        JButton preisB = new JButton("PREIS");
+        JButton ezB = new JButton("EZ");
+        JButton kmB = new JButton("KM");
 
         // Anlegen von RadioButtons
-        JRadioButton nameR = new JRadioButton();
-        JRadioButton preisR = new JRadioButton();
-        JRadioButton ezR = new JRadioButton();
-        JRadioButton kmR = new JRadioButton();
+        JRadioButton nameR = new JRadioButton("NAME");
+        JRadioButton preisR = new JRadioButton("PREIS");
+        JRadioButton ezR = new JRadioButton("EZ");
+        JRadioButton kmR = new JRadioButton("KM");
 
         // Anlegen einer RadioButtonGruppe
         ButtonGroup radio = new ButtonGroup();
@@ -102,10 +111,21 @@ public class Anzeige{
         radio.add(kmR);
 
         // Anlegen von CheckBoxen
-        JCheckBox nameC = new JCheckBox();
-        JCheckBox preisC = new JCheckBox();
-        JCheckBox ezC = new JCheckBox();
-        JCheckBox kmC = new JCheckBox();
+        JCheckBox nameC = new JCheckBox("NAME");
+        JCheckBox preisC = new JCheckBox("PREIS");
+        JCheckBox ezC = new JCheckBox("EZ");
+        JCheckBox kmC = new JCheckBox("KM");
+
+        // BackgroundColor für die Panel
+        nameR.setBackground(Color.LIGHT_GRAY);
+        preisR.setBackground(Color.LIGHT_GRAY);
+        ezR.setBackground(Color.LIGHT_GRAY);
+        kmR.setBackground(Color.LIGHT_GRAY);
+        nameC.setBackground(Color.LIGHT_GRAY);
+        preisC.setBackground(Color.LIGHT_GRAY);
+        ezC.setBackground(Color.LIGHT_GRAY);
+        kmC.setBackground(Color.LIGHT_GRAY);
+        unten.setBackground(Color.GRAY);
 
         // Anlegen einer CheckBoxGruppe
         ButtonGroup check = new ButtonGroup();
@@ -120,37 +140,192 @@ public class Anzeige{
         links.add(ezC);
         links.add(kmC);
 
-        rechts.add(nameC);
-        rechts.add(preisC);
-        rechts.add(ezC);
-        rechts.add(kmC);
+        rechts.add(nameR);
+        rechts.add(preisR);
+        rechts.add(ezR);
+        rechts.add(kmR);
 
-        //Elemente dem Fenster hinzufügen
+        unten.add(nameB);
+        unten.add(preisB);
+        unten.add(ezB);
+        unten.add(kmB);
+
+        // Elemente dem Fenster hinzufügen
         window.add(listeSrcoller,BorderLayout.CENTER);
         window.add(links,BorderLayout.WEST);
         window.add(rechts,BorderLayout.EAST);
-        window.add(unten,BorderLayout.SOUTH);
-
-        // Zur Veranschaulichung einen Border
-        Border border = new LineBorder(Color.black);
+        window.add(unten, BorderLayout.SOUTH);
 
         // Erstellung einer Menüleiste
         JMenuBar bar = new JMenuBar();
 
-        // Wir setzen unsere Umrandung für unsere JMenuBar
-        bar.setBorder(border);
+        // Umrandung für die JMenuBar
+        bar.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+        bar.setBackground(Color.gray);
 
-        // Erzeugung eines Objektes der Klasse JMenu
-        JMenu ansicht=new JMenu("Ansicht");
+        // Erzeugung eines Menüpunktes
+        JMenu ansicht = new JMenu("Ansicht");
+        JMenu bearbeiten = new JMenu("Bearbeiten");
+        JMenu auswahl = new JMenu("Auswahl");
 
-        // Erzeugung eines Objektes der Klasse JSeparator
-        JSeparator sep1=new JSeparator();
+        // Textfarbe ändern
+        ansicht.setForeground(Color.WHITE);
+        bearbeiten.setForeground(Color.WHITE);
+        auswahl.setForeground(Color.WHITE);
+
+        // Erzeugung eines Trenners (Trennstrich)
+        JSeparator sep1 = new JSeparator();
+        JSeparator sep2 = new JSeparator();
+        JSeparator sep3 = new JSeparator();
+        JSeparator sep4 = new JSeparator();
+
+        // Farbe vom Trenner festlegen
+        sep1.setBackground(Color.WHITE);
+        sep2.setBackground(Color.WHITE);
+        sep3.setBackground(Color.WHITE);
+        sep4.setBackground(Color.WHITE);
+
+        // MenüItems erstellen
+        JMenuItem name = new JMenuItem("Name");
+        JMenuItem preis = new JMenuItem("Preis");
+        JMenuItem ez = new JMenuItem("EZ");
+        JMenuItem km = new JMenuItem("Km");
+
+        // BackgroundColor und Textfarbe für die MenüItems festlegen
+        name.setBackground(Color.BLACK);
+        name.setForeground(Color.WHITE);
+        preis.setBackground(Color.BLACK);
+        preis.setForeground(Color.WHITE);
+        ez.setBackground(Color.BLACK);
+        ez.setForeground(Color.WHITE);
+        km.setBackground(Color.BLACK);
+        km.setForeground(Color.WHITE);
+
+        // MenüItem dem Menüpunkt hinzufügen
+        ansicht.add(name);
+        ansicht.add(sep1);
+        ansicht.add(preis);
+        ansicht.add(sep2);
+        ansicht.add(ez);
+        ansicht.add(sep3);
+        ansicht.add(km);
+
+        // JCheckBoxMenuItem erstellen
+        JCheckBoxMenuItem nameMC = new JCheckBoxMenuItem("Name");
+        JCheckBoxMenuItem preisMC = new JCheckBoxMenuItem("Preis");
+        JCheckBoxMenuItem ezMC = new JCheckBoxMenuItem("EZ");
+        JCheckBoxMenuItem kmMC = new JCheckBoxMenuItem("Km");
+
+        // Anlegen einer JCheckBoxMenüItem ButtonGruppe
+        ButtonGroup jcheck = new ButtonGroup();
+        jcheck.add(nameMC);
+        jcheck.add(preisMC);
+        jcheck.add(ezMC);
+        jcheck.add(kmMC);
+
+        // BackgroundColor und Textfarbe für die JCheckBoxMenuItem festlegen
+        nameMC.setBackground(Color.BLACK);
+        nameMC.setForeground(Color.WHITE);
+        preisMC.setBackground(Color.BLACK);
+        preisMC.setForeground(Color.WHITE);
+        ezMC.setBackground(Color.BLACK);
+        ezMC.setForeground(Color.WHITE);
+        kmMC.setBackground(Color.BLACK);
+        kmMC.setForeground(Color.WHITE);
+
+        // JRadioButtonMenuItem erstellen
+        JRadioButtonMenuItem nameMR = new JRadioButtonMenuItem ("Name");
+        JRadioButtonMenuItem preisMR = new JRadioButtonMenuItem("Preis");
+        JRadioButtonMenuItem ezMR = new JRadioButtonMenuItem("EZ");
+        JRadioButtonMenuItem kmMR = new JRadioButtonMenuItem("Km");
+
+        // Anlegen einer JRadioButtonMenuItem ButtonGruppe
+        ButtonGroup jradio = new ButtonGroup();
+        jradio.add(nameMR);
+        jradio.add(preisMR);
+        jradio.add(ezMR);
+        jradio.add(kmMR);
+
+        // BackgroundColor und Textfarbe für die JRadioButtonMenuItem festlegen
+        nameMR.setBackground(Color.BLACK);
+        nameMR.setForeground(Color.WHITE);
+        preisMR.setBackground(Color.BLACK);
+        preisMR.setForeground(Color.WHITE);
+        ezMR.setBackground(Color.BLACK);
+        ezMR.setForeground(Color.WHITE);
+        kmMR.setBackground(Color.BLACK);
+        kmMR.setForeground(Color.WHITE);
+
+        // JRadioButtonMenuItem und JCheckBoxMenuItem dem Menüpunkt hinzufügen
+        bearbeiten.add(nameMC);
+        bearbeiten.add(preisMC);
+        bearbeiten.add(ezMC);
+        bearbeiten.add(kmMC);
+        bearbeiten.add(sep4);
+        bearbeiten.add(nameMR);
+        bearbeiten.add(preisMR);
+        bearbeiten.add(ezMR);
+        bearbeiten.add(kmMR);
+
+        // UnterMenü für Auswahl erzeugen
+        JMenu nameM = new JMenu("Name");
+        JMenu preisM = new JMenu("Preis");
+        JMenu ezM = new JMenu("EZ");
+        JMenu kmM = new JMenu("Km");
+
+        // JRadioButtonMenuItem und JCheckBoxMenuItem für das UnterMenü erzeugen
+        JRadioButtonMenuItem aufNR = new JRadioButtonMenuItem ("aufsteigend");
+        JRadioButtonMenuItem abNR = new JRadioButtonMenuItem("absteigend");
+        JCheckBoxMenuItem aufPC = new JCheckBoxMenuItem("aufsteigend");
+        JCheckBoxMenuItem abPC = new JCheckBoxMenuItem("absteigend");
+        JRadioButtonMenuItem aufER = new JRadioButtonMenuItem ("aufsteigend");
+        JRadioButtonMenuItem abER = new JRadioButtonMenuItem("absteigend");
+        JCheckBoxMenuItem aufKC = new JCheckBoxMenuItem("aufsteigend");
+        JCheckBoxMenuItem abKC = new JCheckBoxMenuItem("absteigend");
+
+        // Anlegen einer JRadioButtonMenuItem ButtonGruppe
+        ButtonGroup jradioNR = new ButtonGroup();
+        ButtonGroup jCheckPC = new ButtonGroup();
+        ButtonGroup jradioER = new ButtonGroup();
+        ButtonGroup jCheckKC = new ButtonGroup();
+        jradioNR.add(aufNR);
+        jradioNR.add(abNR);
+        jCheckPC.add(aufPC);
+        jCheckPC.add(abPC);
+        jradioER.add(aufER);
+        jradioER.add(abER);
+        jCheckKC.add(aufKC);
+        jCheckKC.add(abKC);
+
+        // JRadioButtonMenuItem und JCheckBoxMenuItem dem UmterMenü hinzufügen
+        nameM.add(aufNR);
+        nameM.add(abNR);
+        preisM.add(aufPC);
+        preisM.add(abPC);
+        ezM.add(aufER);
+        ezM.add(abER);
+        kmM.add(aufKC);
+        kmM.add(abKC);
+
+        // UnterMenü dem Menüpunkt Bearbeiten hinzufügen
+        auswahl.add(nameM);
+        auswahl.add(sep1);
+        auswahl.add(preisM);
+        auswahl.add(sep2);
+        auswahl.add(ezM);
+        auswahl.add(sep3);
+        auswahl.add(kmM);
 
         // Menü wird der Menüleiste hinzugefügt
-
+        bar.add(ansicht);
+        bar.add(bearbeiten);
+        bar.add(auswahl);
 
         // Fenstergrösse festlegen
-        window.setSize(600,600);
+        window.setSize(700,600);
+
+        // Festergrösse fest
+        window.setResizable(false);
 
         //Fester position festlegen
         window.setLocationRelativeTo(window);
