0ed0128067c81aeed59477f2e5928f5ce07df9b6
Flurry2010 <flurry2010@gmail.com>
2016-01-19 12:35:11 +0100

    PuzzelHA

--- src/GUI/HACal.java
+++ src/GUI/HACal.java
@@ -1,40 +1,54 @@
 package GUI;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
-/**
- * Created by dfleuren on 15.01.2016.
- */
 public class HACal {
 
-    JTextField eingabe = new JTextField(20);
-    JButton ok = new JButton("OK");
-    JLabel tag = new JLabel();
+    private static final String[] DAYS = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
 
-    public HACal(){
-        JFrame frame = new JFrame("HA Calender");
+    public HACal() {
+        JFrame frame = new JFrame("HA Kalender");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GregorianCalendar cal = new GregorianCalendar();
+        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
         JPanel backg = new JPanel(new FlowLayout());
+        JTextField eingabe = new JTextField(10);
+       // JFormattedTextField eingabe = new JFormattedTextField(new Date());
+        eingabe.setText(df.format(cal.getTime()));
+        JLabel tag = new JLabel(DAYS[cal.get(Calendar.DAY_OF_WEEK)-1]);
+        JButton ok = new JButton("OK");
+        ok.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    cal.setTime(df.parse(eingabe.getText()));
+                   tag.setText(DAYS[cal.get(Calendar.DAY_OF_WEEK) - 1]);
+                }catch (ParseException e1){
+                    JOptionPane.showMessageDialog(frame,"Die Eingabe hat das falsche Format: TT.MM.JJJJ", "Fehler im Datum",JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
         backg.add(eingabe);
         backg.add(ok);
         backg.add(tag);
 
-        frame.add(backg,BorderLayout.NORTH);
+        frame.add(backg, BorderLayout.NORTH);
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
     }
 
-    public String calc (int x){
-
-
-        return "x";
-    }
-
-
     public static void main(String[] args) {
         new HACal();
     }
-
 }
